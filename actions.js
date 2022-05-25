bbf171f2da79ce2d24d63fe4ff84658bda8f607d
akashdan02 <103628569+akashdan02@users.noreply.github.com>
2022-05-25 09:58:48 +0530

    initial commit

---
+++ actions.js
@@ -0,0 +1,181 @@
+(function() {    
+  var dialog1 = document.getElementById('adoption1');    
+  document.getElementById('adoptbutton1').onclick = function() {    
+    dialog1.show();  
+    document.getElementById('closee1').onclick = function() {    
+      dialog1.close();    
+    };  
+}; 
+})();
+(function() {    
+  var dialog2 = document.getElementById('adoption2');    
+  document.getElementById('adoptbutton2').onclick = function() {    
+    dialog2.show();  
+    document.getElementById('closee2').onclick = function() {    
+      dialog2.close();    
+    };  
+}; 
+})();
+(function() {    
+  var dialog1 = document.getElementById('adoption3');    
+  document.getElementById('adoptbutton3').onclick = function() {    
+    dialog1.show();  
+    document.getElementById('closee3').onclick = function() {    
+      dialog1.close();    
+    };  
+}; 
+})();
+(function() {    
+  var dialog2 = document.getElementById('adoption4');    
+  document.getElementById('adoptbutton4').onclick = function() {    
+    dialog2.show();  
+    document.getElementById('closee4').onclick = function() {    
+      dialog2.close();    
+    };  
+}; 
+})();
+(function() {    
+  var dialog1 = document.getElementById('adoption5');    
+  document.getElementById('adoptbutton5').onclick = function() {    
+    dialog1.show();  
+    document.getElementById('closee5').onclick = function() {    
+      dialog1.close();    
+    };  
+}; 
+})();
+(function() {    
+  var dialog2 = document.getElementById('adoption6');    
+  document.getElementById('adoptbutton6').onclick = function() {    
+    dialog2.show();  
+    document.getElementById('closee6').onclick = function() {    
+      dialog2.close();    
+    };  
+}; 
+})();
+(function() {    
+  var dialog2 = document.getElementById('adoption7');    
+  document.getElementById('adoptbutton7').onclick = function() {    
+    dialog2.show();  
+    document.getElementById('closee7').onclick = function() {    
+      dialog2.close();    
+    };  
+}; 
+})();
+(function() {    
+  var dialog2 = document.getElementById('adoption8');    
+  document.getElementById('adoptbutton8').onclick = function() {    
+    dialog2.show();  
+    document.getElementById('closee8').onclick = function() {    
+      dialog2.close();    
+    };  
+}; 
+})();
+(function() {    
+  var dialog1 = document.getElementById('myfirstdialog');    
+  document.getElementById('adoptbtn1').onclick = function() {    
+    dialog1.show();  
+    document.getElementById('close1').onclick = function() {    
+        dialog1.close();    
+      };  
+}; 
+})();
+(function() {    
+    var dialog2= document.getElementById('myseconddialog');    
+    document.getElementById('adoptbtn2').onclick = function() {    
+      dialog2.show();
+      document.getElementById('close2').onclick = function() {    
+        dialog2.close();    
+      };  
+         
+  }; 
+  })();
+  (function() {    
+    var dialog3= document.getElementById('mythirddialog');    
+    document.getElementById('adoptbtn3').onclick = function() {    
+      dialog3.show();
+      document.getElementById('close3').onclick = function() {    
+        dialog3.close();    
+      };  
+         
+  }; 
+  })();
+
+  (function() {    
+    var dialog1= document.getElementById('myfourthdialog');    
+    document.getElementById('adoptbtn4').onclick = function() {    
+      dialog1.show();
+      document.getElementById('close4').onclick = function() {    
+        dialog1.close();    
+      };  
+         
+  }; 
+  })();
+
+  (function() {    
+    var dialog1= document.getElementById('myfifthdialog');    
+    document.getElementById('adoptbtn5').onclick = function() {    
+      dialog1.show();
+      document.getElementById('close5').onclick = function() {    
+        dialog1.close();    
+      };  
+         
+  }; 
+  })();
+
+  (function() {    
+    var dialog1= document.getElementById('mysixthdialog');    
+    document.getElementById('adoptbtn6').onclick = function() {    
+      dialog1.show();
+      document.getElementById('close6').onclick = function() {    
+        dialog1.close();    
+      };  
+         
+  }; 
+  })();
+
+  (function() {    
+    var dialog1= document.getElementById('myseventhdialog');    
+    document.getElementById('adoptbtn7').onclick = function() {    
+      dialog1.show();
+      document.getElementById('close7').onclick = function() {    
+        dialog1.close();    
+      };  
+         
+  }; 
+  })();
+
+  (function() {    
+    var dialog1= document.getElementById('myeighthdialog');    
+    document.getElementById('adoptbtn8').onclick = function() {    
+      dialog1.show();
+      document.getElementById('close8').onclick = function() {    
+        dialog1.close();    
+      };  
+         
+  }; 
+  })();
+
+  (function() {    
+    var dialog1= document.getElementById('myseconddialog');    
+    document.getElementById('adoptbtn2').onclick = function() {    
+      dialog1.show();
+      document.getElementById('close2').onclick = function() {    
+        dialog1.close();    
+      };  
+         
+  }; 
+  })();
+  (function() {    
+    var dialog1= document.getElementById('myseconddialog');    
+    document.getElementById('adoptbtn2').onclick = function() {    
+      dialog1.show();
+      document.getElementById('close2').onclick = function() {    
+        dialog1.close();    
+      };  
+         
+  }; 
+  })();
+
+
+
+  
